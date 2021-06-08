47519901d0472fe68c1b2ccf7fed748c53b410c2
Samy <samygeorge007@gmail.com>
2021-06-09 00:58:30 +0200

    feat: Now listening properly to screen size updates and passing new data that re-renders the chart properly

--- src/components/charts-containers/ReviewsScoreToTime.js
+++ src/components/charts-containers/ReviewsScoreToTime.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'react-responsive';
 import styled from 'styled-components';
 import BarChart from '../reusable/charts/BarChart';
@@ -8,32 +8,32 @@ const ReviewsScoreToTime = () => {
     const [endDate, setEndData] = useState(new Date());
     const [reviewsScoreToTimeBarData] = useState([]);
     const [chartXPoints] = useState(10)
-    const [chartSize, setCharSize] = useState(300)
+    const [currentScreenSize, setCurrentScreenSize] = useState()
     const [tempReviewsScoreToTimeBarData, setTempReviewsScoreToTimeBarData] = useState([...chartOriginalData]);
 
-    const isDesktopSize = useMediaQuery({
-        query: '(min-width: 768px)'
-    })
-    const isTabletSize = useMediaQuery({
-        query: '(min-width: 600px)'
-    })
-    const isMobileSize = useMediaQuery({
-        query: '(max-width: 599px)'
-    })
+    const isDesktop = useMediaQuery({ minWidth: 992 })
+    const isTablet = useMediaQuery({ minWidth: 600, maxWidth: 991 })
+    const isMobile = useMediaQuery({ maxWidth: 600 })
+
+    const updateReviewsScorePointsCount = (pointsCountRequired) => {
+        if(pointsCountRequired !== tempReviewsScoreToTimeBarData.count){
+            setTempReviewsScoreToTimeBarData(chartOriginalData.slice(0, pointsCountRequired));
+        }
+    }
 
     useEffect(() => {
-        if(isMobileSize){
-            console.log('Mobile');
-            setTempReviewsScoreToTimeBarData(oldState => [...chartOriginalData].slice(0, 4));
-            setCharSize(200);
-        }else if(isTabletSize){
-            setTempReviewsScoreToTimeBarData(oldState => [...chartOriginalData].slice(0, 6));
-            console.log('Tablet');
-        }else if(isDesktopSize){
-            console.log('Desktop')
-            setTempReviewsScoreToTimeBarData(oldState => [...chartOriginalData]);
+        console.log('change')
+        if(isMobile){
+            setCurrentScreenSize('Mobile');
+            updateReviewsScorePointsCount(4);
+        }else if(isTablet){
+            setCurrentScreenSize('Tablet');
+            updateReviewsScorePointsCount(6);
+        }else if(isDesktop){
+            setCurrentScreenSize('Desktop');
+            updateReviewsScorePointsCount(10);
         }
-    }, [])
+    }, [isMobile, isTablet, isDesktop, setCurrentScreenSize])
    
     return (
         <ReviewScoreToTimeDiv>
@@ -81,7 +81,7 @@ const DatesRangeDiv = styled.div({
 let chartOriginalData = [
     {
         x: 'Oct',
-        y: 31
+        y: 20
     },
     {
         x: 'Nov',
@@ -89,19 +89,7 @@ let chartOriginalData = [
     },
     {
         x: 'Jan',
-        y: 40
-    },
-    {
-        x: 'Jan',
-        y: 41
-    },
-    {
-        x: 'Jan',
-        y: 25
-    },
-    {
-        x: 'Dec',
-        y: 5
+        y: 18
     },
     {
         x: 'Jan',
@@ -113,23 +101,23 @@ let chartOriginalData = [
     },
     {
         x: 'Jan',
-        y: 64
+        y: 41
     },
     {
         x: 'Jan',
         y: 7
     },
     {
-        x: 'Oct',
-        y: 13
+        x: 'Jan',
+        y: 25
+    },
+    {
+        x: 'Dec',
+        y: 5
     },
     {
         x: 'Jan',
-        y: 11
-    },
-    {
-        x: 'Jan',
-        y: 22
+        y: 64
     },
     {
         x: 'Jan',
